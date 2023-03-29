526c081ad02a3e86f57f127c5f836680dce6e2a6
Tim Ledsam <tim.ledsam@intelligentgrowthsolutions.com>
2023-03-29 12:22:01 +0100

    Fix main

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ def main():
     page = Page()
     page.create_window()
 
-# TODO wire back in
-def getVoltage():
-    q = connect.Quantum()
-    print(q.read_voltage())
-
-def main():
     q = connect.Quantum()
 
     while True:
