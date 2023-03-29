1ca1ce737979d66d1f4e95147c8b1b1977ed6241
Tim Ledsam <tim.ledsam@intelligentgrowthsolutions.com>
2023-03-29 13:35:09 +0100

    Adding take readings button

--- frames/railway.py
+++ frames/railway.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 class Railway():
     def __init__(self):
@@ -8,6 +9,7 @@ class Railway():
     def create_railway_frame(self, root: Tk, tab_control):
         railway_frame = self.add_frame(root)
         self.add_label(railway_frame)
+        self.add_take_readings_button(railway_frame)
         return railway_frame
 
     def add_frame(self, root: Tk):
@@ -19,4 +21,15 @@ class Railway():
         label.grid(column=0,
                    row=0,
                    padx=30,
-                   pady=30)
+                   pady=10)
+
+    def add_take_readings_button(self, frame: Frame):
+        button = ttk.Button(frame, text="Take readings", command=self.take_readings)
+        button.grid(column=0,
+                   row=1,
+                   padx=30,
+                   pady=10)
+
+    def take_readings(self):
+        # TODO replace with instruction call
+        messagebox.showinfo("Button pressed", "Button pressed")
