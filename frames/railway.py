007f0815eaed6d07dd4bf89016c59c0e661be45b
Tim Ledsam <tim.ledsam@intelligentgrowthsolutions.com>
2023-03-29 12:35:53 +0100

    Moving tabs to page and rails to railway

--- frames/railway.py
+++ frames/railway.py
@@ -7,16 +7,8 @@ class Railway():
 
     def create_railway_frame(self, root: Tk, tab_control):
         railway_frame = self.add_frame(root)
-        tab_control.add(railway_frame, text='Railway')
-        self.add_label(railway_frame)
+        return railway_frame
 
     def add_frame(self, root: Tk):
         frame = Frame(root)
         return frame
-
-    def add_label(self, frame: Frame):
-        label = ttk.Label(frame, text="Example tab")
-        label.grid(column=0,
-                   row=0,
-                   padx=30,
-                   pady=30)
--- page.py
+++ page.py
@@ -8,13 +8,23 @@ class Page():
 
     def create_window(self):
         root = Tk()
-        self.add_tab_control(root)
+        self.add_tabs(root)
         root.mainloop()
 
-    def add_tab_control(self, root: Tk):
+    def add_tabs(self, root: Tk):
         tab_control = ttk.Notebook(root)
+
         rail_gen = railway.Railway()
-        rail_gen.create_railway_frame(root, tab_control)
+        railway_frame = rail_gen.create_railway_frame(root, tab_control)
+        self.add_label(railway_frame)
+        tab_control.add(railway_frame, text='Railway')
 
         tab_control.pack(expand=1, fill='both')
+
+    def add_label(self, frame: Frame):
+        label = ttk.Label(frame, text="Example tab")
+        label.grid(column=0,
+                   row=0,
+                   padx=30,
+                   pady=30)
 
